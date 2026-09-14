7c19f7d699e74b319452188d0085427b345a08cd
prufptnv-dotcom <prufptnv@gmail.com>
2026-09-14 21:04:35 +0530

    feat(chat): mount universal dock on dashboard

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,12 +2,13 @@ import "@/styles/globals.css";
 import { ToastProvider } from "../context/ToastContext";
 import { SocketProvider } from "../context/SocketContext";
 import { ModeProvider } from "../context/ModeContext";
-
-
+import { useRouter } from "next/router";
 
 import ErrorBoundary from "../components/ErrorBoundary";
+import UniversalChatDock from "../components/chat/UniversalChatDock";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
 
   return (
     <ErrorBoundary>
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }) {
         <SocketProvider>
           <ModeProvider>
             <Component {...pageProps} />
+            {router.pathname === '/dashboard' && <UniversalChatDock />}
           </ModeProvider>
         </SocketProvider>
       </ToastProvider>
